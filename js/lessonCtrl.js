d803b2e06646e1d04392b1a2bd585b47ed2c3635
Zolboo <admin@zolboo.cf>
2018-06-23 13:16:21 +0800

    Made lessons page on AngularJS

---
+++ js/lessonCtrl.js
@@ -0,0 +1,14 @@
+function lessonCtrl($scope) {
+    $scope.showPanel = function(event) {
+        var clickedElement = angular.element(event.target);
+        clickedElement.toggleClass("active");
+
+        var panel = clickedElement.next();
+        var style = window.getComputedStyle(panel[0]);
+        if (style.getPropertyValue('max-height') === '0px') {
+            panel.css('max-height', panel[0].scrollHeight + 'px');
+        } else {
+            panel.css('max-height', '0px');
+        }
+    }
+}
